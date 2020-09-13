563e828f086cf268ffe6ea4f7f088f53583c5e8e
kawabacchan <daisuke30x@gmail.com>
2020-09-13 17:18:55 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,　presense: true
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ンー-龥]+\z/} do
+    validates :family_name,　presense: true
+    validates :first_name,　presense: true
+  end
+  with_options presence: true, format: {with: /\A[ァ-ン]+\z/} do
+    validates :family_kananame,　presense: true
+    validates :first_kananame,　presense: true
+  end
+  validates :birthday,　presense: true
 end
