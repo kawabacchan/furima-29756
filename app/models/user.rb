f289928e986a2f22573712d2bd87700f15eee0c5
kawabacchan <daisuke30x@gmail.com>
2020-09-14 12:07:50 +0900

    passwordを英字混合、エラー文記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,16 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :birthday, presence: true
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
     validates :family_name
     validates :first_name
   end
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/} do
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用してください'} do
     validates :family_kananame
     validates :first_kananame
   end
 
-  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください'
+
 end
