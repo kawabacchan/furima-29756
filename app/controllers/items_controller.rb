06a291a90f7354f3bce81564aeee3fa00ba99a1a
kawabacchan <daisuke30x@gmail.com>
2020-09-14 21:04:38 +0900

    itemsテーブルのバリデーションの設定とimageカラムの紐付け

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,10 @@ class ItemsController < ApplicationController
 
   def index
   end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :content, :category, :item_status, :delivery_fee, :seller_area, :days_to_ship, :price, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :category, presence: true
+  validates :item_status, presence: true
+  validates :delivery, presence: true
+  validates :seller_area, presence: true
+  validates :days_to_ship, presence: true
+  validates :price, presence: true
+
 end
