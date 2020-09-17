bf04e272e94e6ec8c54c0f8bc0dae38449522ee2
kawabacchan <daisuke30x@gmail.com>
2020-09-17 14:25:16 +0900

    購入ページに遷移する時、未ログインユーザーはログインページへ、出品者はトップページへ遷移させる設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,18 @@
 class OrdersController < ApplicationController
-  
+  before_action :authenticate_user!
+  before_action :move_to_root
+
   def index
     @order = Order.new
     @item = Item.find(params[:item_id])
+  end
+
+  private
+  
+  def move_to_root
+    if user_signed_in? && current_user.id == Item.find(params[:item_id]).user_id
+      redirect_to root_path
+    end
   end
 
 end
